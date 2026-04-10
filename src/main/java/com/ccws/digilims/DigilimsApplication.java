9d92d093d1f0198e7de86819458c4f61316cca9b
tharindung <gtnadeesha@yahoo.com>
2026-04-10 10:05:59 -0400

    Initial commit

---
+++ src/main/java/com/ccws/digilims/DigilimsApplication.java
@@ -0,0 +1,24 @@
+package com.ccws.digilims;
+
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigilimsApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(DigilimsApplication.class);
+
+	@PostConstruct
+	public void init()
+	{
+		logger.info("Started Application");
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigilimsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ccws/digilims/DigilimsApplicationTests.java
@@ -0,0 +1,24 @@
+package com.ccws.digilims;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigilimsApplicationTests {
+
+	private static final Logger logger = LoggerFactory.getLogger(DigilimsApplication.class);
+
+	@Test
+	void contextLoads() {
+
+		logger.info("Test executed");
+
+		assertEquals(true, true);
+
+	}
+
+}
